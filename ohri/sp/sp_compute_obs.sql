9302c35af03a65f32833a56864561b53bdb5ee8a
smallGod <davies.mugume@gmail.com>
2022-07-17 13:01:28 +0300

    Refactor/cleanup

--- ohri/sp/sp_compute_obs.sql
+++ ohri/sp/sp_compute_obs.sql
@@ -11,13 +11,11 @@ CREATE PROCEDURE sp_compute_obs(
 BEGIN
     -- DECLARE handler_name VARCHAR(38) DEFAULT CONCAT('handle', event_type);
     SET @compute_obs_procedure_sql = CONCAT('CALL ', procedure_name, '(?,?,?);');
-
-    PREPARE prepared_statement FROM @compute_obs_procedure_sql;
-
     SET @obs_encounter_id = obs_encounter_id;
     SET @obs_concept_id = obs_concept_id;
     SET @person_id = person_id;
 
+    PREPARE prepared_statement FROM @compute_obs_procedure_sql;
     EXECUTE prepared_statement USING @obs_encounter_id, @obs_concept_id, @person_id;
     DEALLOCATE PREPARE prepared_statement;
 END
